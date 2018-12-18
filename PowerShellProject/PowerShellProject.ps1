2015ce01a20995aba65f7cef8a00b1b8a6b71d80
Simon Oliver Olavus <simols10@ostfoldfk.no>
2018-12-18 19:59:13 +0100

    Script for kalenderrettigheter

---
+++ PowerShellProject/PowerShellProject.ps1
@@ -0,0 +1,20 @@
+﻿#
+# Script.ps1
+#
+
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session -DisableNameChecking -AllowClobber
+
+
+get-aduser -Filter {Department -eq 'KARSEN Karrieresenter Østfold' -and SamAccountName -ne 'username'} | % {    
+    "$($_.SamAccountName):\kalender"
+    Remove-MailboxFolderPermission -Identity "$($_.SamAccountName):\kalender" -User username -Confirm $false
+}
+
+
+get-aduser -Filter {Department -eq 'KARSEN Karrieresenter Østfold' -and SamAccountName -ne 'username'} | % {
+	 "$($_.SamAccountName):\kalender"
+    Add-MailboxFolderPermission -Identity "$($_.SamAccountName):\kalender" -User username -AccessRights Editor -SharingPermissionFlags Delegate
+}
+
